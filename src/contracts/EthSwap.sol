a11f67923e504fd3167be3967e8829475a83d267
RobinD57 <robin.davidseth@gmail.com>
2020-07-03 12:41:29 +0200

    continued tests and integration of EthSwap smart contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,33 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol";
+
 contract EthSwap {
     string public name = "EthSwap instant exchange";
+    Token public token;
+    uint public rate = 100; // how many tokens you get per ETH
+
+    event TokenPurchase(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        rate;
+        uint tokenAmount = msg.value * rate;
+
+        // this = EthSwap's address
+        require(token.balanceOf(address(this)) >= tokenAmount);
+        
+        token.transfer(msg.sender, tokenAmount);
+
+        // event that tokens were purchased
+        emit TokenPurchase(msg.sender, address(token), tokenAmount, rate);
+    }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -9,13 +9,13 @@ function tokens(n) {
     return web3.utils.toWei(n, "ether");
 }
 
-contract("EthSwap", (accounts) => {
+contract("EthSwap", ([deployer, investor]) => {
     let token, ethSwap
 
     before(async () => {
         token = await Token.new()
-        ethSwap = await EthSwap.new()
-        await token.transfer(ethSwap.address, tokens(1000000))
+        ethSwap = await EthSwap.new(token.address)
+        await token.transfer(ethSwap.address, tokens("1000000"))
     })
 
     describe("Token deployment", async () => {
@@ -33,7 +33,32 @@ contract("EthSwap", (accounts) => {
 
         it("contract has tokens", async () => {
             let balance = await token.balanceOf(ethSwap.address)
-            assert.equal(balance.toString(), tokens(1000000))
+            assert.equal(balance.toString(), tokens("1000000"))
+        })
+    })
+
+    describe("buyTokens()", async () => {
+        let result
+
+        before(async () => {
+            result = await ethSwap.buyTokens({ from: investor, value: web3.utils.toWei("1", "ether") })
+        })
+
+        it("allows user to instantly purchase tokens from EthSwap for a fixed price", async () => {
+            // investor received tokens
+            let investorBalance = await token.balanceOf(investor)
+            assert.equal(investorBalance.toString(), tokens("100"))
+
+            let ethSwapBalance = await token.balanceOf(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), tokens("999900"))
+            ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), web3.utils.toWei("1", "Ether"))
+
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount.toString(), tokens("100").toString())
+            assert.equal(event.rate.toString(), "100")
         })
     })
 })
